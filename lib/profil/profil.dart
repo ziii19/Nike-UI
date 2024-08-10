c97489f79d75cf470ab6e8d0a06a212d3330e0a7
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-08-10 12:39:39 +0700

    fix unused import

--- lib/profil/profil.dart
+++ lib/profil/profil.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nike/home/drawer.dart';
-import 'package:nike/onboard.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
